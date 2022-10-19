6db3cf1b8d192214bfb286be6ad6a32ddc3580cb
Asad Mahmood <asadmhmd1234@gmail.com>
2022-10-20 01:44:54 +0530

    Finish exercise 2

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 const persons = [
-	{
-		"id": 1,
-		"name": "Arto Hellas",
-		"number": "040-123456"
-	},
-	{
-		"id": 2,
-		"name": "Ada Lovelace",
-		"number": "39-44-5323523"
-	},
-	{
-		"id": 3,
-		"name": "Dan Abramov",
-		"number": "12-43-234345"
-	},
-	{
-		"id": 4,
-		"name": "Mary Poppendieck",
-		"number": "39-23-6423122"
-	}
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (req, res) => {
-	return res.json(persons)
-})
+app.get("/api/persons", (req, res) => {
+  return res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  return res.send(
+    `Phonebook has info for ${
+      persons.length
+    } people ${new Date().toUTCString()}`
+  );
+});
 
 app.listen(PORT, () => {
-	console.log('I am listening on port 3001')
-})
+  console.log("I am listening on port 3001");
+});
